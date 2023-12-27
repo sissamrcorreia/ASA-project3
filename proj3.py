273fa5462b9bc4c0a72697c59b9cc89600d60a28
sissamrcorreia <115619893+sissamrcorreia@users.noreply.github.com>
2023-12-27 22:08:24 +0000

    testes do enunciado

--- proj3.py
+++ proj3.py
@@ -5,33 +5,37 @@
 # Luísa Fernandes - 102460
 
 # Importar PuLP modeller functions
-from pulp import LpProblem, LpMaximize, LpVariable
+from pulp import *
+
 
 def calcular_lucro_maximo(n, p, max_brinquedos, brinquedos, pacotes):
     # Criar o problema de programação linear
-    problema = LpProblem("Maximizar_Lucro", LpMaximize)
+    prob = LpProblem("Maximizar_Lucro", LpMaximize)
 
     # Variáveis: quantidade de cada brinquedo a ser produzida
-    quantidades = [LpVariable(f"x{i}", lowBound=0, cat="Integer") for i in range(1, n + 1)]
+    quant = [LpVariable(f"x{i}", lowBound=0, cat="Integer") for i in range(1, n + 1)]
 
-    # Função objetivo: maximizar o lucro total
-    problema += sum(quantidades[i - 1] * brinquedos[i - 1][0] for i in range(1, n + 1)), "Lucro_Total"
+    # Maximizar o lucro total
+    prob += sum(quant[i - 1] * brinquedos[i - 1][0] for i in range(1, n + 1)), "Lucro_Total"
 
     # Restrição de capacidade de produção
-    problema += sum(quantidades[i - 1] * brinquedos[i - 1][1] for i in range(1, n + 1)) <= max_brinquedos, "Capacidade_Producao"
+    prob += sum(quant[i - 1] * brinquedos[i - 1][1] for i in range(1, n + 1)) <= max_brinquedos, "Capacidade_Producao"
 
     # Adicionar restrições dos pacotes especiais
     for i, j, k, lucro in pacotes:
-        problema += quantidades[i - 1] + quantidades[j - 1] + quantidades[k - 1] <= 2, f"Pacote_{i}_{j}_{k}"
+        prob += quant[i - 1] + quant[j - 1] + quant[k - 1] <= 2, f"Pacote_{i}_{j}_{k}"
 
-    problema.solve()
+    prob.solve()
 
     # Retornar lucro máximo
-    return int(problema.objective.value())
+    return int(prob.objective.value())
 
 
 # Ler a entrada
-t, p, max_brinquedos = map(int, input().split()) # map para converter strings em inteiros
+t, p, max_brinquedos = input().split()
+t = int(t) # numero de brinquedos
+p = int(p) # numero de pacotes
+max_brinquedos = int(max_brinquedos) # capacidade máxima de produção
 brinquedos = [tuple(map(int, input().split())) for _ in range(t)]
 pacotes = [tuple(map(int, input().split())) for _ in range(p)]
 
